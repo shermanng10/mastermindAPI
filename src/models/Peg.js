b4e401047c5bd7b1368a97df05e8bf93bda05331
shermanng10 <sherman.ng10@gmail.com>
2016-07-06 16:57:25 -0400

    Add validation of color to Peg class and an InvalidInput class

--- src/models/Peg.js
+++ src/models/Peg.js
@@ -1,5 +1,17 @@
+import {InvalidInput} from '../libs/Exceptions'
+
 export default class Peg {
 	constructor(color){
-		this.color = color
+		this.color = this._validate_color(color)
+	}
+
+	_validate_color(color){
+		let validColors = ["red", "blue", "green", "yellow", "orange", "white"]
+		if (!(validColors.indexOf(color) > -1)){
+			throw new InvalidInput('Not a valid color')
+		}
+		else {
+			return color
+		}
 	}
 }
