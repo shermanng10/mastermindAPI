4b2ac7f33dec6b12d50c45a1c1d9f05146db83eb
shermanng10 <sherman.ng10@gmail.com>
2016-07-07 14:47:29 -0400

    Update formatting on Peg model

--- src/models/Peg.js
+++ src/models/Peg.js
@@ -3,20 +3,19 @@ import {InvalidInput} from '../libs/Exceptions'
 let _color = new WeakMap()
 export default class Peg {
 	constructor(color){
-		_color.set(this, this._validate_color(color))
+		_color.set(this, this._validateColor(color))
+	}
+
+	_validateColor(color){
+		let validColors = ["red", "blue", "green", "yellow", "orange", "white"]
+		if (!(validColors.indexOf(color) > -1)){
+			throw new InvalidInput('Not a valid color')
+		} else {
+			return color
+		}
 	}
 
 	getColor(){
 		return _color.get(this)
-	}
-
-	_validate_color(color){
-		let validColors = ["red", "blue", "green", "yellow", "orange", "white"]
-		if (!(validColors.indexOf(color) > -1)){
-			throw new InvalidInput('Not a valid color')
-		}
-		else {
-			return color
-		}
 	}
 }
