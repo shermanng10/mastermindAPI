0ef3da6507458e263213c0a0ba046508c4317d2d
shermanng10 <sherman.ng10@gmail.com>
2016-07-09 00:50:54 -0400

    Add validation module to handle validation of peg colors, row length, guesses.

---
+++ src/lib/Validations.js
@@ -0,0 +1,35 @@
+ import {InvalidInput} from './Exceptions'
+
+ let validations = (function(validations){
+ 	return {
+ 		validColors: function(){
+ 			return ["red", "blue", "green", "yellow", "purple", "teal"]
+ 		},
+
+	 	validateColors: function(rowArray){
+			let validColors = this.validColors()
+			for (let i = 0; i < rowArray.length; i++){
+				if (!(validColors.indexOf(rowArray[i]) > -1)){
+					console.log(rowArray[i])
+					throw new InvalidInput('Not a valid color.')
+				}
+			}
+			return rowArray
+		},
+
+		validateRowLength: function(rowArray){
+			if (rowArray.length != 4){
+				throw new InvalidInput('You need to have four pegs per row.')
+			} else { 
+				return rowArray
+			}
+		},
+
+		validateGuess: function(rowArray){
+			this.validateColors(rowArray)
+			this.validateRowLength(rowArray)
+		}
+	}
+})();
+
+module.exports = {validations}
